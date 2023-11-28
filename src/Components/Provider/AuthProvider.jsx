4817a00c3fec65a96ee73c0e68f4aae1554d77b5
saikat <tawhidulislam3482@gmail.com>
2023-11-28 14:28:24 +0600

    Implement user registration and authentication

--- src/Components/Provider/AuthProvider.jsx
+++ src/Components/Provider/AuthProvider.jsx
@@ -61,7 +61,7 @@ const AuthProvider = ({ children }) => {
         localStorage.removeItem("access-token")
         setLoading(false);
       }
-      // setLoading(false)
+      // setLoading(false
     });
     return () => {
       return observer();
